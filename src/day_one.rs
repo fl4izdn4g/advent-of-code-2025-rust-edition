32ce341c6daf517a7158d2f5007c70f49a44b38f
fl4izdn4g <lukasz.gorecki@interia.eu>
2026-01-20 22:11:53 +0100

    Refactored to two locking mechanisms.
    Abstraction FTW

--- src/day_one.rs
+++ src/day_one.rs
@@ -8,7 +8,7 @@ fn get_rotations() -> BufReader<File> {
     BufReader::new(file)
 }
 
-pub struct Rotation {
+struct Rotation {
     pub direction: String,
     pub move_by: i16
 }
@@ -22,48 +22,153 @@ impl Rotation {
             move_by
         }
     }
+
+    pub fn direction(&self) -> &String {
+        &self.direction
+    }
+
+    pub fn move_by(&self) -> &i16 {
+        &self.move_by
+    }
 }
 
-pub fn secret_entrance_v1() {
-    // number of times the dial is pointing at 0 after any rotation in sequence
-    let mut password: u16 = 0;
-    let mut current_dial_position: i16 = START_POSITION;
+trait LockingMechanism {
+    fn new(starting_position: i16) -> Self;
+    fn rotate(&mut self, rotation: &Rotation);
+    fn compute_password(&self) -> i16;
+}
 
-    let data = get_rotations();
+struct StandardMechanism {
+    position: i16,
+    password: i16,
+}
 
-    for line in data.lines() {
-        let rotation_operation = line.unwrap();
-        let rotation = Rotation::new(&rotation_operation);
+struct SecuredMechanism {
+    position: i16,
+    password: i16
+}
 
-        let current_move_by = &rotation.move_by % 100;
+impl LockingMechanism for StandardMechanism {
+    fn new(starting_position: i16) -> StandardMechanism {
+        StandardMechanism {
+            position: starting_position,
+            password: 0
+        }
+    }
 
-        if &rotation.direction == "L" {
+    fn rotate(&mut self, rotation: &Rotation) {
+        let current_move_by = rotation.move_by() % 100;
+
+        if rotation.direction() == "L" {
             // minus
-            if current_dial_position - current_move_by < 0 {
-                let rest = current_move_by - current_dial_position;
-                current_dial_position = 100 - rest;
+            if self.position - current_move_by < 0 {
+                let rest = current_move_by - self.position;
+                self.position = 100 - rest;
             } else {
-                current_dial_position -= current_move_by;
+                self.position -= current_move_by;
             }
         } else {
             // plus
-            if current_dial_position + current_move_by > 100 {
-                let rest = current_dial_position + current_move_by - 100;
-                current_dial_position = rest;
+            if self.position + current_move_by > 100 {
+                let rest = self.position + current_move_by - 100;
+                self.position = rest;
             } else {
-                current_dial_position += current_move_by;
+                self.position += current_move_by;
             }
         }
 
-        if (100 - current_dial_position) == 0 || current_dial_position == 0 {
-            current_dial_position = 0;
-            password += 1;
+        if (100 - self.position) == 0 || self.position == 0 {
+            self.position = 0;
+            self.password += 1;
         }
 
-        println!("Rolling for {} -> {}: {} ({}) ==> {}", rotation_operation, &rotation.direction, &rotation.move_by, current_move_by, current_dial_position);
-
-
+        println!("Rolling for {}: {} ({}) ==> {}", rotation.direction(), rotation.move_by(), current_move_by, self.position);
     }
-    println!("Password is {}", password)
 
+    fn compute_password(&self) -> i16 {
+        self.password
+    }
+}
+
+impl LockingMechanism for SecuredMechanism {
+    fn new(starting_position: i16) -> SecuredMechanism {
+        SecuredMechanism {
+            position: starting_position,
+            password: 0i16
+        }
+    }
+
+    fn rotate(&mut self, rotation: &Rotation) {
+        let full_rotations_counter = rotation.move_by().wrapping_div(100);
+        if full_rotations_counter > 0 {
+            println!("<-- full rotation -->");
+            self.password += full_rotations_counter;
+        }
+        println!("Full rotations -> {} => Password after rotation: {}", full_rotations_counter, self.password);
+        let current_move_by = rotation.move_by() % 100;
+
+        if rotation.direction() == "L" {
+            // minus
+            if self.position - current_move_by < 0 {
+                let rest = current_move_by - self.position;
+                self.position = 100 - rest;
+                self.password += 1;
+            } else {
+                self.position -= current_move_by;
+            }
+        } else {
+            // plus
+            if self.position + current_move_by > 100 {
+                let rest = self.position + current_move_by - 100;
+                self.position = rest;
+                self.password += 1;
+            } else {
+                self.position += current_move_by;
+            }
+        }
+
+        if (100 - self.position) == 0 || self.position == 0 {
+            self.position = 0;
+            self.password += 1;
+        }
+
+        println!("Rolling for {}: {} ({}) ==> {}", rotation.direction(), rotation.move_by(), current_move_by, self.position);
+    }
+
+    fn compute_password(&self) -> i16 {
+        self.password
+    }
+}
+
+
+pub fn secret_entrance_v1() {
+    // number of times the dial is pointing at 0 after any rotation in sequence
+    let data = get_rotations();
+
+    let mut standard_lock = StandardMechanism::new(START_POSITION);
+    for line in data.lines() {
+        let rotation_operation = line.unwrap();
+        // println!("For operation: {}", rotation_operation);
+
+        let rotation = Rotation::new(&rotation_operation);
+        standard_lock.rotate(&rotation);
+    }
+    let found_password = standard_lock.compute_password();
+    println!("Found password: {}", found_password)
+}
+
+pub fn second_entrance_v2() {
+    // number of times the dial is passing 0 after any rotation in sequence
+    let data = get_rotations();
+
+    let mut secured_lock = SecuredMechanism::new(START_POSITION);
+    for line in data.lines() {
+        let rotation_operation = line.unwrap();
+        // println!("For operation: {}", rotation_operation);
+
+        let rotation = Rotation::new(&rotation_operation);
+        secured_lock.rotate(&rotation);
+    }
+    let found_password = secured_lock.compute_password();
+    println!("Found password: {}", found_password)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod day_one;
 fn main() {
     println!("Hello, world!");
     day_one::secret_entrance_v1();
+    day_one::second_entrance_v2();
 }
